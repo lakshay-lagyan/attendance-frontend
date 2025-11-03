f3749f1c84245295a4fc7bb204881304440f694c
lakshay-lagyan <lakshaylagyan7@gmail.com>
2025-11-03 19:56:17 +0530

    update

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const API_CONFIG = {
     // Backend API URL 
-    BASE_URL: 'https://attendance-backend-api-production-c552.up.railway.app/',
+    BASE_URL: 'https://attendance-backend-api-production-c552.up.railway.app',
     
     // API endpoints
     ENDPOINTS: {
